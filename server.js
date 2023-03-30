efbb4f8474ea726955e04f4fba39462f8f98c0cf
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-30 00:27:15 -0400

    last one didn't work, on the right track

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import ViteExpress from "vite-express";
 
+
 const app = express();
 ViteExpress.config({ mode: "production" })
 
-app.get("/", (_, res) => res.send(index.html));
+app.get("/", (_, res) => res.send(__dirname, 'public', 'index.html'));
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
