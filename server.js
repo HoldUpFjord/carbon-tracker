9e98900082b8f12f11bc441e6382c6162c710568
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-29 23:56:42 -0400

    trying build first

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import ViteExpress from "vite-express";
 const app = express();
 ViteExpress.config({ mode: "production" })
 
+ViteExpress.build();
 app.get("/message", (_, res) => res.send("Hello from express!"));
 
-app.use(express.static('build'))
+
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
