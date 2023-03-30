3102e50e774d8ae1544a1abe36f43b2a9946410d
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-30 00:35:09 -0400

    closer.....

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ import ViteExpress from "vite-express";
 const app = express();
 ViteExpress.config({ mode: "production" })
 
-app.get("/", (_, res) => res.send( import.meta.url, 'public', 'index.html'));
+app.get("/", (_, res) => res.send( import.meta.url, 'public', index.html));
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
