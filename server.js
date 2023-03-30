dd8eba42529cd6946d748ac251faac7f6fa40fe4
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-29 23:35:14 -0400

    anotha one

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 
 import express from "express";
 import ViteExpress from "vite-express";
-const app = express();
 
-ViteExpress.config({ mode: "production" })
-app.use(express.static('build'))
 const app = express();
 const server = http.createServer(app).listen(3000, () => { 
    console.log("Server is listening!")
