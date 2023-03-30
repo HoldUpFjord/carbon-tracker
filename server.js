14e15d37c0fe1a97ac8b4a68c581ea2e0aecd356
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-30 00:50:04 -0400

    i'm really trying

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import ViteExpress from "vite-express";
-
+const index = fs.readFile(path.resolve("./public/index.html"))
 
 const app = express();
 ViteExpress.config({ mode: "production" })
 
-app.get("/", (_, res) => res.send( import.meta.url, 'build', "./public/index.html"));
+app.get("/", (_, res) => res.send( import.meta.url, 'build', index));
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
