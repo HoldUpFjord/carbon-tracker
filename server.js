6eed502e3e9f985d197f454a52d39928a814c0c2
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-30 00:19:00 -0400

    new thing

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from "express";
 import ViteExpress from "vite-express";
-import * as http from 'http'; 
+
 const app = express();
+ViteExpress.config({ mode: "production" })
 
-ViteExpress.build();
+app.get("/:universalURL", (_, res) => res.send(index.HTML));
 
-const server = http.createServer(app).listen(3000, () => { 
-   console.log("Server is listening!")
-});
-ViteExpress.bind(app, server);
+ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
