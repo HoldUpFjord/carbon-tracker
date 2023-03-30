79ce648ee6c112864b183da1e8bc9aec2aeda6bf
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-30 00:11:21 -0400

    trying vite build again

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import * as http from 'http'; 
-
 const app = express();
+
+ViteExpress.build();
+
 const server = http.createServer(app).listen(3000, () => { 
    console.log("Server is listening!")
 });
