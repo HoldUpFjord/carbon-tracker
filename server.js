9d323b71c063c229b4ee619e2985886fd369f546
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-29 23:59:12 -0400

    trying async http method

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from "express";
 import ViteExpress from "vite-express";
+const http = require('http')
 
 const app = express();
-ViteExpress.config({ mode: "production" })
-
-ViteExpress.build();
-app.get("/message", (_, res) => res.send("Hello from express!"));
-
-
-ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
+const server = http.createServer(app).listen(3000, () => { 
+   console.log("Server is listening!")
+});
+ViteExpress.bind(app, server);
