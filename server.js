3e168ca4aaf9a0df66cd28ac5b48156ca266263e
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-29 23:19:20 -0400

    tryint to get cyclic to run my shit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 
 import express from "express";
 import ViteExpress from "vite-express";
-
 const app = express();
+
 ViteExpress.config({ mode: "production" })
+app.use(express.static('build', options))
 
 app.get("/message", (_, res) => res.send("Hello from express!"));
 
-ViteExpress.listen(app, 3000, () => console.log("Server is listening on 3000..."));
+const port = process.env.PORT || 3000
+ViteExpress.listen(app, port, () => console.log("Server is listening on 3000..."));
