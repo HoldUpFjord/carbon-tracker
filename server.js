8d5918f66bbc094fb8b2afb4393eab9819c2ddb5
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-29 23:54:18 -0400

    .

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ ViteExpress.config({ mode: "production" })
 
 app.get("/message", (_, res) => res.send("Hello from express!"));
 
+app.use(express.static('build'))
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
