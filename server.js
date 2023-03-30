0555bb73cb9c82b0ab8721df6293cc2af6034646
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-30 00:21:58 -0400

    trying new route,

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ import ViteExpress from "vite-express";
 const app = express();
 ViteExpress.config({ mode: "production" })
 
-app.get("/:universalURL", (_, res) => res.send(index.HTML));
+app.get("/", (_, res) => res.send("Hello from express!"));
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
