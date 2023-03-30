382d8e84ca1e0130347e963704910a5955ce3ddb
jackmonaco20 <jackmonaco20@yahoo.com>
2023-03-30 01:16:29 -0400

    trying a workaround

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 
 const app = express();
 ViteExpress.config({ mode: "production" })
-
-app.get("/", (_, res) => res.sendFile(path.join('index.html', { root: import.meta.url })));
+console.log(import.meta.url)
+app.get("/", (_, res) => res.sendFile(path.join(__dirname +'index.html' )));
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
