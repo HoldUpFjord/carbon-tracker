e051755fc459ad0e3dfa027ac776d0623dd70876
jackmonaco20 <jackmonaco20@yahoo.com>
2023-04-01 15:14:22 -0400

    cyclicGPT #1

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 import express from 'express';
-import { createServer as createViteServer } from 'vite';
 
-async function start() {
-  const app = express();
 
-  // Create Vite server in middleware mode
-  const vite = await createViteServer({
-    server: { middlewareMode: true }
-  });
+import path from 'path';
 
-  // Use Vite's middleware
-  app.use(vite.middlewares);
+const app = express();
+const port = 3000;
 
-  // Serve static assets
-  app.use(express.static('public'));
+const staticPath = path.resolve(__dirname, 'dist');
 
-  // Start the server
-  const port = 3000;
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
-}
+app.use(express.static(staticPath));
 
-start();
+app.get('*', (req, res) => {
+  res.sendFile(path.join(staticPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
