e2ff0de0724b8f6abdcbee15714e6f2b0d9b71bd
jackmonaco20 <jackmonaco20@yahoo.com>
2023-04-01 15:16:45 -0400

    combo

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from 'express';
-
-
 import path from 'path';
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const staticPath = path.resolve(__dirname, 'dist');
 
